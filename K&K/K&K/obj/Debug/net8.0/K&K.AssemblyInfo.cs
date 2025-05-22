72c364ebc266a96fa48929da7958011ddad83fcc
maidabiber <mbiber1@etf.unsa.ba>
2025-05-22 13:36:10 +0200

    Dijagram Interakcije

--- K&K/K&K/obj/Debug/net8.0/K&K.AssemblyInfo.cs
+++ K&K/K&K/obj/Debug/net8.0/K&K.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("K&K")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af9548207aa6aa5b0ebb06b8244290c65b2c8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a41e0ad1d7e9fd19f46f0bc7c7a01a1c80f855")]
 [assembly: System.Reflection.AssemblyProductAttribute("K&K")]
 [assembly: System.Reflection.AssemblyTitleAttribute("K&K")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
