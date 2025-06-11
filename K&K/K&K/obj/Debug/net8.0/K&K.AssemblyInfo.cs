e92e098b4327ff12c7b250c706d6728f9986bead
emusinovic1 <emusinovic1@etf.unsa.ba>
2025-06-11 02:33:23 +0200

    pdv

--- K&K/K&K/obj/Debug/net8.0/K&K.AssemblyInfo.cs
+++ K&K/K&K/obj/Debug/net8.0/K&K.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("K&K")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a4b9f56fe9dba2476b8f71beb2e75c895bc2079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093c9f0c4140ce67a9340fc74cb87a0b9b2a499c")]
 [assembly: System.Reflection.AssemblyProductAttribute("K&K")]
 [assembly: System.Reflection.AssemblyTitleAttribute("K&K")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
